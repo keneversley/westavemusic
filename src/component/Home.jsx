fc8e75852e62912392c0130e299d4763aca20d30
keneversley <krazorfbx@gmail.com>
2025-04-21 10:19:40 -0700

    video

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -20,6 +20,8 @@ const Home = () => {
         autoPlay
         muted
         loop
+        playsInline
+        preload="auto"
         className="absolute top-0 left-0 w-full h-full object-cover z-0"
       >
         <source src="/clip.mp4" type="video/mp4" />
